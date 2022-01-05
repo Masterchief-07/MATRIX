fb9e6214f77ec871acb92720c0f9acdf0b18fe23
Masterchief-07 <kjonathan1999@hotmail.com>
2022-01-05 23:16:20 +0000

    templatisation of Matrix

--- Matrix.hpp
+++ Matrix.hpp
@@ -11,6 +11,8 @@
 #include <vector>
 #include <cassert>
 #include <iostream>
+
+template<typename T>
 class Matrix
 {
 	public:
@@ -20,17 +22,106 @@ class Matrix
 		~Matrix();
 
 		//getting value in the matrix
-		double at(int pos);
-		double at(int row, int colomn);
+		T at(int pos);
+		T at(int row, int colomn);
 
 		//setting value in the matrix
 		void set(int pos, double value);
 		void set(int row, int column, double value);
+
+		//void printArray
+		void print();
 	private:
 		int m_dimX, m_dimY;
-		std::vector<double> *m_matrix;
+		std::vector<T> *m_matrix;
 
 		//comput position
 		int getPos(int x);
 		int getPos(int row, int column);
 };
+
+
+
+template<typename T>
+Matrix<T>::Matrix()
+{
+	m_matrix = new std::vector<T>(1);
+}
+
+
+template<typename T>
+Matrix<T>::Matrix(int dim): m_dimX(1), m_dimY(dim)
+{
+	assert(dim>0);
+	m_matrix = new std::vector<T>(dim, 99999.f);
+	std::cout<<"m_matrix.size: "<<m_matrix.size()<<"\n";	
+}
+
+
+template<typename T>
+Matrix<T>::Matrix(int dimX, int dimY):m_dimX(dimX), m_dimY(dimY)
+{
+	assert(dimX>0 &&  dimY>0);
+	m_matrix = new std::vector<T>(dimY * dimX, 0.f);
+	std::cout<<"m_matrix.size: "<<m_matrix->size()<<"\n";
+}
+
+//destructor
+template<typename T>
+Matrix<T>::~Matrix()
+{
+	delete m_matrix;
+}
+
+//comput the position
+template<typename T>
+int Matrix<T>::getPos(int row, int column)
+{
+	assert(row<m_dimX && column<m_dimY);
+	return row*(m_dimY) + column;
+}
+
+template<typename T>
+int Matrix<T>::getPos(int pos)
+{
+	assert(pos<m_dimY);
+	return pos;
+}
+
+//get data at position x
+template<typename T>
+T Matrix<T>::at(int pos)
+{
+	return m_matrix->at(getPos(pos));
+}
+
+template<typename T>
+T Matrix<T>::at(int posX, int posY)
+{
+	return m_matrix->at(getPos(posX, posY)); 
+
+}
+
+//setting data at position
+
+template<typename T>
+void Matrix<T>::set(int pos, double value)
+{
+	(*m_matrix)[getPos(pos)] = value;
+}
+
+template<typename T>
+void Matrix<T>::set(int row, int column, double value)
+{
+	(*m_matrix)[getPos(row, column)] = value;
+}
+
+
+//print array
+template<typename T>
+void Matrix<T>::print()
+{
+	for(auto const& i: (*m_matrix))
+		std::cout<<i<<" ";
+	std::cout<<"\n";
+}
