846d87b9a0222057982e82790f7ecd01e9bc5efe
Masterchief-07 <kjonathan1999@hotmail.com>
2023-06-09 02:04:12 +0000

    init testing

---
+++ tests/tests_matrix2d.cpp
@@ -0,0 +1,10 @@
+#include <catch2/catch_test_macros.hpp>
+// #include <catch2/benchmark/catch_benchmark.hpp>
+#include <matrix2d.hpp>
+// #include <linear.hpp>
+
+TEST_CASE("test MATRIX2D", "[MATRIX2D]") {
+
+    // matrix::Matrix2D<double> myVect{};
+   
+}
---
+++ tests/tests_matrix3d.cpp
@@ -0,0 +1,9 @@
+#include <catch2/catch_test_macros.hpp>
+// #include <catch2/benchmark/catch_benchmark.hpp>
+#include <matrix3d.hpp>
+// #include <linear.hpp>
+
+TEST_CASE("test MATRIX3D", "[MATRIX3D]") {
+    matrix::Matrix3D<double> myVect{};
+   
+}
