d6d3f4b780de52efd43210926a4e71e60fbccc50
Masterchief-07 <kjonathan1999@hotmail.com>
2023-06-11 07:23:13 +0000

    added news tests

--- tests/tests_matrix2d.cpp
+++ tests/tests_matrix2d.cpp
@@ -58,6 +58,15 @@ TEST_CASE("test MATRIX2D", "[MATRIX2D]") {
         }
     }
 
+    SECTION("AT")
+    {
+        matrix::Matrix2D<double> mat{2,2, {1,2,3,4}};
+        matrix::Matrix1D<double> result{{1,2}};
+        matrix::Matrix1D<double> result2{{3,4}};
+        REQUIRE(mat.at(0) == result);
+        REQUIRE(mat.at(1) == result2);
+    }
+
     SECTION("Transpose")
     {
         matrix::Matrix2D<double> mat{2, 3, {1,2,3,4,5,6}};
@@ -75,7 +84,9 @@ TEST_CASE("test MATRIX2D", "[MATRIX2D]") {
         matrix::Matrix2D<double> result{2, 2, {(1*1+2*3), (1*2+2*4), (3*1+4*3), (3*2+4*4)}};
         REQUIRE(mat.dot(mat) == result);
         mat = matrix::Matrix2D<double>{matrix::Matrix1D<double>{{1,2,3,4,5}}};
+        auto matInt = matrix::Matrix2D<int>{matrix::Matrix1D<int>{{1,2,3,4,5}}};
         REQUIRE(mat.dot(mat.Tr()) == matrix::Matrix2D<double>{1,1,(1+4+9+16+25)});
+        REQUIRE(mat.dot(matInt.Tr()) == matrix::Matrix2D<double>{1,1,(1+4+9+16+25)});
     }
    
     SECTION("matrix to matrix operation")
@@ -91,9 +102,23 @@ TEST_CASE("test MATRIX2D", "[MATRIX2D]") {
         REQUIRE(mat == result*mat2);
         mat /= mat2;
         REQUIRE(mat == (result*mat2)/mat2);
-
+        
     }
 
+    SECTION("MATRIX TO MATRIX OPERATION DIFFERENTS TYPES"){
+        // -----------------------------------------------------
+        auto mat = matrix::Matrix2D<int> {10,10, 2};
+        auto mat2 = matrix::Matrix2D<double> {10,10, 3.3};
+        auto result = matrix::Matrix2D<int> {10,10, 2};
+        mat += mat2;
+        REQUIRE(mat == result+mat2);
+        mat -= mat2;
+        REQUIRE(mat == (result+mat2)-mat2);
+        mat *= mat2;
+        REQUIRE(mat == result*mat2);
+        mat /= mat2;
+        REQUIRE(mat == (result*mat2)/mat2);
+    }
     SECTION("matrix to scalar operation")
     {
        matrix::Matrix2D<double> mat{10,10, 2.3};
@@ -106,6 +131,20 @@ TEST_CASE("test MATRIX2D", "[MATRIX2D]") {
         REQUIRE(mat == result*3);
         mat /= 3;
         REQUIRE(mat == (result*3)/3);
+
+    }
+    SECTION("matrix to scalar DIFFERENT VALUE operation")
+    {
+       matrix::Matrix2D<int> mat{10,10, 2};
+        matrix::Matrix2D<double> result{10,10, 2};
+        mat += 3.75;
+        REQUIRE(mat == result+3.75);
+        mat -= 3.75;
+        REQUIRE(mat == (result+3.75)-3.75);
+        mat *= 3.75;
+        REQUIRE(mat == result*3.75);
+        mat /= 3.75;
+        REQUIRE(mat == (result*3.75)/3.75);
 
     }
 }
