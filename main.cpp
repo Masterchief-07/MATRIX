4e829bbf9cd9bfa481d0985630689285a14c9b95
Masterchief-07 <kjonathan1999@hotmail.com>
2023-06-12 04:00:20 +0000

    testing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <matrix1d.hpp>
+#include <matrix2d.hpp>
 int main()
 {
+    matrix::Matrix1D<int> mat1d{{1, 2,3,4,5,6,7,8}};
+    std::cout<<mat1d<<"\n";
+    matrix::Matrix2D<int> mat2d{2,4,{1,2,3,4,5,6,7,8}};
+    std::cout<<mat2d<<"\n";
+    std::cout<<mat2d+mat1d<<"\n";
     return 0;
 }
